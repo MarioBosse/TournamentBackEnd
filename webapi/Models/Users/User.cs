1fb2c805c394947a71629a88c43e98a84d6b1b95
Mario Bossé <mariobosse007@gmail.com>
2023-06-18 21:26:32 -0400

    ControlerLogin
    Login/All
    Login/Check
    Login/Email

--- webapi/Models/Users/User.cs
+++ webapi/Models/Users/User.cs
@@ -14,15 +14,13 @@ namespace webapi.Models.Users
         public string Gender { get; set; } = string.Empty;
         [Required]
         public string Password { get; set; } = string.Empty;
-        public string ProfilePhoto { get; set; } = string.Empty;
+        public string? ProfilePhoto { get; set; } = string.Empty;
         public bool IsActivated { get; set; } = false;
-        public string PasswordResetCode { get; set; } = string.Empty;
-        public string ActivationCode { get; set; } = string.Empty;
-        public string RememberToken { get; set; } = string.Empty;
-        public Int64 IdAddress { get; set; } = 0;
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
-
-        public virtual Address.Address Adresse { get; set; }
+        public string? PasswordResetCode { get; set; } = string.Empty;
+        public string? ActivationCode { get; set; } = string.Empty;
+        public string? RememberToken { get; set; } = string.Empty;
+        public Int64? IdAddress { get; set; } = 0;
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
     }
 }
