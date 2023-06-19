a4fe38c341468a3da46d62652cda4ccc6a188706
Mario Bossé <mariobosse007@gmail.com>
2023-06-18 23:16:17 -0400

    Ajouter les valeurs par défaut dans le BD

---
+++ webapi/Controllers/GalleryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Context;
+using webapi.Models.Users;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    public class GalleryController : ControllerBase
+    {
+        private readonly UserRoleContext _roleContext;
+        public GalleryController(UserRoleContext roleContext)
+        {
+            _roleContext = roleContext;
+        }
+
+        [Route("[controler]/All")]
+        public List<String> GetAlls()
+        {
+            List<User> usr = new DbLink.Login(_roleContext).GetAlls();
+            List<String> result = new List<String>();
+
+            foreach(var user in usr)
+            {
+                result.Add(user.ProfilePhoto);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ webapi/DbLink/Login.cs
@@ -0,0 +1,44 @@
+﻿using webapi.Context;
+using webapi.Models;
+using webapi.Models.Users;
+
+namespace webapi.DbLink
+{
+    public class Login
+    {
+        private readonly UserRoleContext _roleContext;
+        public Login(UserRoleContext roleContext)
+        {
+            _roleContext = roleContext;
+        }
+
+        public Boolean IsLoggedIn
+        {
+            get;
+        }
+
+        public List<User> GetAlls()
+        {
+            if (_roleContext == null || _roleContext.Users == null) return new List<User>();
+
+            return _roleContext.Users.Where(e => e.IdUser > 0).ToList<User>();
+
+        }
+        public Boolean EmailExisting(String email)
+        {
+            if(_roleContext == null || _roleContext.Users == null) return false;
+
+            return _roleContext.Users.Where(e => e.Email == email).Any();
+        }
+
+        public User? CheckConnection(LoginSend loginSend)
+        {
+            if (_roleContext == null || _roleContext.Users == null) return null;
+
+            var val = _roleContext.Users.Where(e => e.Email == loginSend.Email && e.Password == loginSend.Password).FirstOrDefault();
+
+            // Création d'un Token et enregistrement de celui-ci
+            return val;
+        }
+    }
+}
