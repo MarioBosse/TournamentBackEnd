af7685472c4d68a8baf9530dab7b7d408ad87495
Mario Bossé <mariobosse007@gmail.com>
2023-06-23 23:55:52 -0400

    Birthdate
    Ajouter la date de naissance

--- webapi/Models/Repository/Login/AllUsers.cs
+++ webapi/Models/Repository/Login/AllUsers.cs
@@ -6,6 +6,7 @@
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Gender { get; set; } = string.Empty;
+        public DateTime? Birthdate { get; set; }
         public byte[]? ProfilePhoto { get; set; }
         public bool IsActivated { get; set; } = false;
         public Complements.Address? Address { get; set; } = null;
